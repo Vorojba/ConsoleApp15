11f7774faa36405830e92873509f853492040e58
Tester <test7@user.com>
2021-10-19 12:15:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x ;//присваивание переменной
+            Console.Write("Введите x =");
+            x = Convert.ToDouble(Console.ReadLine());//конвертирование
+            if (x < -2)//условие выполняется
+                Console.WriteLine("y =" + 2 * Math.Log10(x + 2));//вывод на экран
+            if ((-2 <= x) && (x < 5))//условие выполняется
+                Console.WriteLine("y =" +Math.Sqrt(x + 2 * Math.Sqrt(x + 3)));//вывод на экран
+            if (x >= 5)//условие выполняется
+                Console.WriteLine("y =" +(3 * Math.Pow(x, 2) - 5 * x - 3));//вывод на экран
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
